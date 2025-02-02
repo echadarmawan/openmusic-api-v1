ea99282b3939f2b62028cd2c68170551cd68e47d
echadarmawan <keshiadavinapd@gmail.com>
2025-02-02 18:17:25 +0700

    submission

---
+++ migrations/1738307509509_add-foreign-key-albumId.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.addConstraint('songs', 'fk_songs_album', {
+    foreignKeys: {
+      columns: 'albumId',
+      references: 'albums(id)',
+      onDelete: 'CASCADE'
+    }
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropConstraint('songs', 'fk_songs_album');
+};
